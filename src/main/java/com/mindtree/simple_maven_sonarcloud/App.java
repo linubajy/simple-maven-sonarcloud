909ac5762ce8386ccd54ffeca9e173dc2f048cb7
linubajy <blinu.1997@gmail.com>
2021-02-21 18:28:49 +0530

    First commit

---
+++ src/main/java/com/mindtree/simple_maven_sonarcloud/App.java
@@ -0,0 +1,13 @@
+package com.mindtree.simple_maven_sonarcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
